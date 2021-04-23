79fd29021f2a82a4f3324a042b74a311374bf249
AhmedAdel21 <ahmed12365488@gmail.com>
2021-04-23 23:31:44 +0200

    active contour

--- Task2/task2.py
+++ Task2/task2.py
@@ -10,7 +10,7 @@ import matplotlib as plt
 import random
 from UI import Ui_MainWindow
 from lines_hough import hough_lines
-
+import snake as sn
 # from collections import Counter # Replaced
 
 
@@ -32,14 +32,65 @@ class GUI(Ui_MainWindow):
             self.images[i].view.setRange(xRange=[0,100],yRange=[0,100], padding=0)
 
         #retrieve the original image data
-        hough_lines("linesInput.jpg")
+        # hough_lines("linesInput.jpg")
+
+        #   Active contour
+        self.snakeContour()
         
         
     
     
         
 ######################################################################################################
+#       DoLa
+    def snakeContour(self):
+        img = np.load('./img.npy')
+        t = np.arange(0, 2*np.pi, 0.1)
+        x = 120+50*np.cos(t)
+        y = 140+60*np.sin(t)
 
+        alpha = 0.001
+        beta = 0.4
+        gamma = 100
+        iterations = 50
+
+        # fx and fy are callable functions
+        fx, fy = sn.create_external_edge_force_gradients_from_img( img )
+
+        snakes = sn.iterate_snake(
+            x = x,
+            y = y,
+            a = alpha,
+            b = beta,
+            fx = fx,
+            fy = fy,
+            gamma = gamma,
+            n_iters = iterations,
+            return_all = True
+        )
+        self.activeContoursInputImage.setImage(img,xvals=np.linspace(1., 3., img.shape[0]))
+        # self.activeContoursOutputImage.setImage(img,xvals=np.linspace(1., 3., img.shape[0]))
+
+        fig = plt.pyplot.figure()
+        ax  = fig.add_subplot()
+        ax.imshow(img)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_xlim(0,img.shape[1])
+        ax.set_ylim(img.shape[0],0)
+        ax.plot(np.r_[x,x[0]], np.r_[y,y[0]], c=(0,1,0), lw=2)
+
+        for i, snake in enumerate(snakes):
+            if i % 10 == 0:
+                ax.plot(np.r_[snake[0], snake[0][0]], np.r_[snake[1], snake[1][0]], c=(0,0,1), lw=2)
+
+        # Plot the last one a different color.
+        ax.plot(np.r_[snakes[-1][0], snakes[-1][0][0]], np.r_[snakes[-1][1], snakes[-1][1][0]], c=(1,0,0), lw=2)
+
+        plt.pyplot.savefig('snake.jpg')
+        outImg = cv.imread('./snake.jpg')
+        self.activeContoursOutputImage.setImage(outImg)
+######################################################################################################
 
 
 if __name__ == "__main__":
