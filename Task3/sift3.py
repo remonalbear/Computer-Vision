102ea56e66e7d8653eb7cbc71d9984521d8a68ae
Almotasem100 <mohammed.almotasem55@googlemail.com>
2021-05-07 23:11:19 +0200

    lena_approach

--- Task3/sift3.py
+++ Task3/sift3.py
@@ -197,19 +197,6 @@ def local_descriptors(img,key_points):
 
 
 
-# gray = np.float32(gray)
-# dst = cv.cornerHarris(gray,2,3,0.04)
-# temp=np.zeros(gray.shape)
-# #result is dilated for marking the corners, not important
-# dst = cv.dilate(dst,None)
-# # Threshold for an optimal value, it may vary depending on the image.
-# img[dst>0.01*dst.max()]=[0,255,0]
-# cv.imshow('dst',img)
-# if cv.waitKey(0) & 0xff == 27:
-#     cv.destroyAllWindows()
-
-
-
 filename = 'cat.jpg'
 img = cv.imread(filename)
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
