c63b96e7fd3c708837f63d303830b0c786700159
Almotasem100 <mohammed.almotasem55@googlemail.com>
2021-05-17 06:45:26 +0200

    task4

---
+++ Task4/threshold.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2 
+from otsu import otsu_threshold
+from optimal import optimal_threshold
+
+
+def Global_threshold(image , thresh_typ = "Optimal"):
+    if len(image.shape) > 2:
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    if thresh_typ == "Otsu":
+        threshold = otsu_threshold(image)
+    elif thresh_typ == "Spectral":
+        do_this =0
+    else:
+        threshold = optimal_threshold(image)
+
+    thresh_img = np.where(image > threshold, 255, 0)
+    return thresh_img
+
+def Local_threshold(image, block_size , thresh_typ = "Optimal"):
+    if len(image.shape) > 2:
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    thresh_img = np.copy(image)
+    for row in range(0, image.shape[0], block_size):
+        for col in range(0, image.shape[1], block_size):
+            mask = image[row:(row+block_size,image.shape[0]),col:min(col+block_size,image.shape[1])]
+            thresh_img[row:(row+block_size,image.shape[0]),col:min(col+block_size,image.shape[1])] = Global_threshold(mask, thresh_typ)
+    return thresh_img
+
+
+
+
+source_image = cv2.imread("lena.jpg", 0)
